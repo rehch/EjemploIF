98b782606077e3cd8aced75f9c01c9783c23cf71
rhernandezch <rhernandezch@yahoo.com>
2021-09-08 18:32:32 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EjemploIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal ValorDeLaPrenda = 0.00M;
+            Console.WriteLine("Ingrese el valor de la prenda");
+            string ValorenPantalla = Console.ReadLine();
+            
+            ValorDeLaPrenda =  Convert.ToDecimal( ValorenPantalla);
+
+            //int descuento = ValorDeLaPrenda > 150 ? 12 : 5;
+            int descuento = 0;
+
+            if(ValorDeLaPrenda > 150)
+            {
+                descuento = 12;
+                Console.WriteLine("me gane un superdescuento");
+
+            }
+            else
+            {
+                descuento = 5;
+                Console.WriteLine("el desuentoe es normal");
+            }
+
+            Console.WriteLine("Ulitma linea");
+
+        }
+    }
+}
